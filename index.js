ef3b2819dc1608f451d32490e43aaeda979f1d9c
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-22 09:53:32 +0100

    added:// tested and works well!

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const exphbs=require('express-handlebars');
 const {allowInsecureProtypeAccess}=require('@handlebars/allow-prototype-access');
 const bodyparser=require('body-parser');
 
-const app= express();
+//app
+var app= express();
+
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+//
+app.get('/',(req, res)=>{
+    res.send(`
+      <h2>Welcome to students database!</h2>
+      <h3>Click to get access to the link <b><a href="/students/list">Database</a> </b></h3>
+    `);
+});
+//server listener port
 const port=(process.env.PORT||3000)
 app.listen(port, (req,res)=>{
     console.log("Server started at port: "+port);
